a887deec5a04da5ea3e8f64fbb8cd80859d03edf
marytc <mari75479248@gmail.com>
2024-09-20 22:08:18 -0400

    raycast presentado

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -6,13 +6,13 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Controller2D : MonoBehaviour
 {
-    public LayerMask collisionMask;
+    public LayerMask collisionMask;     // Máscara de colisión para detectar qué objetos 
 
-    const float skinWidth = .015f;
-    public int horizontalRayCount = 4;
-    public int verticalRayCount = 4;
+    const float skinWidth = .015f;      // Ancho del raycast
+    public int horizontalRayCount = 4;  // Número de rayos horizontales
+    public int verticalRayCount = 4;    // Número de rayos verticales
 
-    float horizontalRaySpacing;
+    float horizontalRaySpacing; // Espaciado entre rayos horizontales
     float verticalRaySpacing;
 
     BoxCollider2D collider;
@@ -22,71 +22,79 @@ public class Controller2D : MonoBehaviour
     void Start()
     {
         collider = GetComponent<BoxCollider2D>();
-        CalculateRaySpacing();
+        CalculateRaySpacing();  // Calcula el espaciado de los rayos
     }
 
+    // Metodo para mover al jugador
     public void Move(Vector3 velocity)
     {
         UpdateRaycastOrigins();
         collisions.Reset();
 
+        // Verifica si hay movimiento horizontal
         if (velocity.x != 0)
         {
-            HorizontalCollisions(ref velocity);
+            HorizontalCollisions(ref velocity);     // Gestiona colisiones Horizontales
         }
+        // Verifica si hay movimiento vertical
         if (velocity.y != 0)
         {
-            VerticalCollisions(ref velocity);
+            VerticalCollisions(ref velocity);       // Gestiona colisiones Verticales
         }
 
-        transform.Translate(velocity);
+        transform.Translate(velocity);      // Mueve al jugador
     }
 
+    // Gestiona colisiones Horizontales
     void HorizontalCollisions(ref Vector3 velocity)
     {
-        float directionX = Mathf.Sign(velocity.x);
-        float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+        float directionX = Mathf.Sign(velocity.x);              // Determina la dirección del movimiento horizontal
+        float rayLength = Mathf.Abs(velocity.x) + skinWidth;    // Longitud del rayo
 
+        // muestra los rayos Horizontales
         for (int i = 0; i < horizontalRayCount; i++)
         {
-            Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
-            rayOrigin += Vector2.up * (horizontalRaySpacing * i);
-            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+            Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;    // Establece el origen del rayo en función de la dirección del movimiento
+            rayOrigin += Vector2.up * (horizontalRaySpacing * i);       // Ajusta la posición del rayo
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);  // Lanza el rayo
 
-            Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
+            Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);    // Dibuja el rayo rojos para depuración
 
+            // Si hay una colisión
             if (hit)
             {
-                velocity.x = (hit.distance - skinWidth) * directionX;
-                rayLength = hit.distance;
+                velocity.x = (hit.distance - skinWidth) * directionX;   // Calcula la velocidad
+                rayLength = hit.distance;   // Actualiza la longitud del rayo
 
-                collisions.left = directionX == -1;
-                collisions.right = directionX == 1;
+                collisions.left = directionX == -1; // Actualiza el estado de colisión
+                collisions.right = directionX == 1; // Actualiza el estado de colisión
             }
         }
     }
 
     void VerticalCollisions(ref Vector3 velocity)
     {
-        float directionY = Mathf.Sign(velocity.y);
-        float rayLength = Mathf.Abs(velocity.y) + skinWidth;
+        float directionY = Mathf.Sign(velocity.y);  // Determina la dirección del movimiento
+        float rayLength = Mathf.Abs(velocity.y) + skinWidth;    // Longitud del rayo
 
+        // genera de rayos verticales
         for (int i = 0; i < verticalRayCount; i++)
         {
-            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
-            rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;    // Calcula el origen del rayo en función de la dirección del movimiento
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x); // Ajusta la posición del rayo
             RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
-            Debug.DrawRay(raycastOrigins.bottomLeft + Vector2.right * verticalRaySpacing * i, Vector2.up * -2, Color.red);
+            Debug.DrawRay(raycastOrigins.bottomLeft + Vector2.right * verticalRaySpacing * i, Vector2.up * -2, Color.red);  // Dibuja los rayos rojos para depuración
 
             //Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
 
+            // Si hay una colisión
             if (hit)
             {
-                velocity.y = (hit.distance - skinWidth) * directionY;
-                rayLength = hit.distance;
+                velocity.y = (hit.distance - skinWidth) * directionY;   // Calcula la velocidad
+                rayLength = hit.distance;   // Actualiza la longitud del rayo
 
-                collisions.below = directionY == -1;
-                collisions.above = directionY == 1;
+                collisions.below = directionY == -1;    // Actualiza el estado de colisión
+                collisions.above = directionY == 1;     // Actualiza el estado de colisión
             }
         }
     }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,20 +15,20 @@ public class Player : MonoBehaviour
     Vector3 velocity;
     float velocityXSmoothing;
 
-    Controller2D controller;
+    Controller2D controller;        // Control raycast y colisiones
 
     void Start()
     {
         controller = GetComponent<Controller2D>();
 
-        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
-        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
-        print("Gravity: " + gravity + "  Jump Velocity: " + jumpVelocity);
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2); // Calcula gravedad para que el jugador pueda alcanzar una altura máxima al saltar
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;         // La velocidad de salto se determina en función de la gravedad
+        print("Gravity: " + gravity + "  Jump Velocity: " + jumpVelocity);  // Imprime en la consola los valores calculados de gravedad y velocidad de salto para depuración
     }
 
     void Update()
     {
-
+        // Resetea la velocidad vertical si hay colisión arriba o abajo
         if (controller.collisions.above || controller.collisions.below)
         {
             velocity.y = 0;
@@ -36,14 +36,17 @@ public class Player : MonoBehaviour
 
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
+        // Maneja el salto
         if (Input.GetKeyDown(KeyCode.Space) && controller.collisions.below)
         {
-            velocity.y = jumpVelocity;
+            velocity.y = jumpVelocity;      // Asigna la velocidad de salto
         }
 
-        float targetVelocityX = input.x * moveSpeed;
-        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+        float targetVelocityX = input.x * moveSpeed;    // Determina la velocidad deseada en el eje X según la entrada del jugador
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);  // Suaviza la transición entre la velocidad actual y la velocidad objetivo
+
+        velocity.y += gravity * Time.deltaTime;     // Aplica la gravedad al jugador
+        controller.Move(velocity * Time.deltaTime); // Actualiza la posición del jugador según la velocidad calculada
+
     }
 }
